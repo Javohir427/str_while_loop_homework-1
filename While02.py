e1439783245cad3fc509ac140e306272e2d3bca7
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-29 10:11:25 +0500

    02

--- While02.py
+++ While02.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a = 0
+    for i in s:
+        if i.isalpha():
+            a+=1
+
+    return a
+print(main('PYThon    '))
