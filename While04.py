eb531d5a847481f2eb19cdb64e7c3f6eb763cb86
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-29 13:28:29 +0500

    04

--- While04.py
+++ While04.py
@@ -6,4 +6,9 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a = 0
+    for i in s:
+        if i.isupper():
+            a+=1
+    return a
+print(main("CodeschoolUz"))
