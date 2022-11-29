b99bf768c21cc06bd9ffae037917bf867a426b6d
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-29 13:25:28 +0500

    03

--- While03.py
+++ While03.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a = 0
+    for i in s:
+        if i.isalpha() or i.isdigit():
+            a+=0
+        else :
+            a+=1
+    return a
+print(main("#hashtag@$"))
